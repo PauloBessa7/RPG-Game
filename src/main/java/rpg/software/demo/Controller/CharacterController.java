c863f65d06ab28e0a164fc23b5fa999b4661ffa2
Paulo Henrique Basso Bessa <paulo.bessa@nipponflex.com>
2025-04-09 17:24:18 -0300

    Add routers in swagger and modified models

--- src/main/java/rpg/software/demo/Controller/CharacterController.java
+++ src/main/java/rpg/software/demo/Controller/CharacterController.java
@@ -1,8 +1,7 @@
 package rpg.software.demo.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,60 +10,274 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import rpg.software.demo.DTO.UpdateNameDTO;
 import rpg.software.demo.Model.Character;
-import rpg.software.demo.Model.MagicItem;
 import rpg.software.demo.Service.CharacterService;
 
 @RestController
 @RequestMapping("/characters")
+@Tag(name = "Characters")
 public class CharacterController {
     
     @Autowired
     private CharacterService characterService;
 
-    @GetMapping("/list")
-    public List<Character> listCharacters() {
-        return characterService.getAllCharacters();
-    }
-
-    @GetMapping("/list/{id}")
-    public Character getCharacterById(@PathVariable Long id) {
-        return characterService.getCharacterById(id);
+    @PostMapping("/update/{id}")
+    @Operation(
+        summary = "Update name if specific Character",
+        parameters = @Parameter(
+            name = "id",
+            required = true,
+            example = "1"
+        ),
+        requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(mediaType = "application/json",
+            examples = @ExampleObject(
+                value = "{\"name\": \"Charlinhos\"}"
+                )
+            )
+        ),
+        responses = @ApiResponse(
+            responseCode = "200",    
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = Character.class)
+            )
+        )
+    )
+    public ResponseEntity<Object> updateCharacterName(@PathVariable Long id, @RequestBody UpdateNameDTO dto) {
+        try {
+            return ResponseEntity.ok(characterService.updateName(id, dto.name()));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
 
     @PostMapping("/create")
-    public Character createCharacter(@RequestBody Character character) {
-        return characterService.createCharacter(character);
-    }
-
-    @DeleteMapping("/remove/{id}")
-    public Boolean removeCharacter(@PathVariable Long id) {
-        return characterService.removeCharacter(id);
-    }
-
-    @PostMapping("/update/{id}")
-    public Character updateCharacterName(@PathVariable Long id, @RequestBody UpdateNameDTO dto) {
-        return characterService.updateName(id, dto.name());
+    @Operation(
+        summary = "Create a character",
+        requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                mediaType = "application/json",
+                examples = @ExampleObject(
+                    "{\"name\": \"Charlinhoso\", \"namePlayer\": \"Tenebroso\", \"characterClass\": \"BARD\", \"level\": \"0\", \"power\": \"2\", \"defence\": \"2\"}" 
+                )
+            )
+        ),
+        responses = {
+            @ApiResponse(
+                content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                    "{\"id\": \"1\", \"name\": \"Charlinhoso\", \"namePlayer\": \"Tenebroso\", \"characterClass\": \"BARD\", \"level\": \"0\", \"power\": \"2\", \"defence\": \"2\"}" 
+                    )      
+                )
+            )
+        }
+    )
+    public ResponseEntity<Object> createCharacter(@RequestBody Character character) {
+        try {
+            return ResponseEntity.ok(characterService.createCharacter(character));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
 
     @PostMapping("/add-magic-item/{idCharacter}/{idMagicItem}")
-    public Character addMagicItem(@PathVariable Long idCharacter, @PathVariable Long idMagicItem) {
-        return characterService.addMagicItem(idCharacter, idMagicItem);
+    @Operation(
+        summary = "Add magic item to character",
+        parameters = {
+            @Parameter(
+                name = "idCharacter",
+                required = true,
+                example = "1"
+            ),
+            @Parameter(
+                name = "idMagicItem",
+                required = true,
+                example = "1"
+            ),
+        },
+        responses = {
+            @ApiResponse(
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Character.class)
+                )
+            )
+        }
+    )
+    public ResponseEntity<Object> addMagicItem(@PathVariable Long idCharacter, @PathVariable Long idMagicItem) {
+        try {
+            return ResponseEntity.ok(characterService.addMagicItem(idCharacter, idMagicItem));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+
+
+    @GetMapping("/list")
+    @Operation(
+        summary = "List all characters",
+        responses = @ApiResponse(
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = Character.class)   
+            )
+        )
+    )
+    public ResponseEntity<Object> listCharacters() {
+        try {
+            return ResponseEntity.ok(characterService.getAllCharacters());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/list/{id}")
+    @Operation(
+        summary = "Get specific characters",
+        parameters = @Parameter(
+            required = true,
+            name = "id",
+            example = "1"
+        ),
+        responses = @ApiResponse(
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = Character.class)   
+            )
+        )
+    )
+    public ResponseEntity<Object> getCharacterById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(characterService.getCharacterById(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
 
     @GetMapping("/list-magic-items/{idCharacter}")
-    public List<MagicItem> listMagicItems(@PathVariable Long idCharacter) {
-        return characterService.listMagicItemsByCharacterId(idCharacter);
-    }
-
-    @DeleteMapping("/remove-magic-item/{idCharacter}/{idMagicItem}")
-    public Character removeMagicItem(@PathVariable Long idCharacter, @PathVariable Long idMagicItem) {
-        return characterService.removeMagicItem(idCharacter, idMagicItem);	
+    @Operation(
+        summary = "List all magic items if specific character",
+        parameters = @Parameter(
+            required = true,
+            name = "idCharacter",
+            example = "1"
+        ),
+        responses = {
+            @ApiResponse(
+                content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                        "[{\"id\": \"1\", \"name\": \"Diamond Armor\", \"type\": \"ARMOR\", \"power\": \"0\", \"defence\": \"2\"}]"
+                    )
+                )
+            )
+        }
+    )
+    public ResponseEntity<Object> listMagicItems(@PathVariable Long idCharacter) {
+        try {
+            return ResponseEntity.ok(characterService.listMagicItemsByCharacterId(idCharacter));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
 
     @GetMapping("list-amulets/{idCharacter}")
-    public Character listAmulets(@PathVariable Long idCharacter) {
-        return characterService.getAmulet(idCharacter);
+    @Operation(
+        summary = "list amulet if specific character",
+        parameters = @Parameter(
+            required = true,
+            name = "idCharacter",
+            example = "1"
+        ),
+        responses = {
+            @ApiResponse(
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                        implementation = Character.class
+                    )
+                )
+            )
+        }
+    )
+    public ResponseEntity<Object> listAmulets(@PathVariable Long idCharacter) {
+        try {
+            return ResponseEntity.ok(characterService.getAmulet(idCharacter));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
+
+    @DeleteMapping("/remove/{id}")
+    @Operation(
+        summary = "delete specific character",
+        parameters = @Parameter(
+            required = true,
+            name = "id",
+            example = "1"
+        ),
+        responses = {
+            @ApiResponse(
+                content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                        "true"
+                    )
+                )
+            )
+        }
+    )
+    public ResponseEntity<Object> removeCharacter(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(characterService.removeCharacter(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+
+
+    @DeleteMapping("/remove-magic-item/{idCharacter}/{idMagicItem}")
+    @Operation(
+        summary = "Remote specific magic item in specific character",
+        parameters = {
+            @Parameter(
+                required = true,
+                name = "idCharacter",
+                example = "1"
+            ),
+            @Parameter(
+                required = true,
+                name = "idMagicItem",
+                example = "1"
+            )
+        },
+        responses = {
+            @ApiResponse(
+                content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                        "{\"name\": \"Charlinhoso\", \"namePlayer\": \"Tenebroso\", \"characterClass\": \"BARD\", \"level\": \"0\", \"power\": \"2\", \"defence\": \"2\"}"
+                    )
+                )
+            )
+        }
+    )
+    public ResponseEntity<Object> removeMagicItem(@PathVariable Long idCharacter, @PathVariable Long idMagicItem) {
+        try {
+            return ResponseEntity.ok(characterService.removeMagicItem(idCharacter, idMagicItem));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }	
+    }
+
 }
--- src/main/java/rpg/software/demo/Controller/MagicItemController.java
+++ src/main/java/rpg/software/demo/Controller/MagicItemController.java
@@ -1,9 +1,9 @@
 package rpg.software.demo.Controller;
 
-import java.util.List;
-
 import rpg.software.demo.Model.MagicItem;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,30 +11,102 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import rpg.software.demo.Service.MagicItemService;
 
 @RestController
 @RequestMapping("/magic-items")
+@Tag(name = "Magic Items", description = "Operations for Magic Items")
 public class MagicItemController {
-    
+
     @Autowired
     private MagicItemService magicItemService;
 
     @GetMapping("/list")
-    public List<MagicItem> listMagicItems() {
-        return magicItemService.getAllMagicItems();
+    @Operation(
+        summary = "List all Magic Items", 
+        description = "Get a list of all magic items",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Item created",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = MagicItem.class)
+                )
+            )
+        }
+    )
+    public ResponseEntity<Object> listMagicItems() {
+        try{
+            return ResponseEntity.ok(magicItemService.getAllMagicItems());
+        } catch(Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
 
     @GetMapping("/list/{id}")
-    public MagicItem getMagicItemById(@PathVariable Long id) {
-        return magicItemService.getMagicItemById(id);
+    @Operation(
+        summary = "List a magic item", 
+        description = "Get a magic item by its specific ID",
+        parameters = @Parameter(
+            name = "id", 
+            required = true, 
+            example = "1" // Valor de exemplo para o parâmetro 'id'
+        ),
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Item created",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = MagicItem.class)
+                )
+            )
+        }
+    )
+    public ResponseEntity<Object> getMagicItemById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(magicItemService.getMagicItemById(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
 
     @PostMapping("/create")
-    public MagicItem createItem(@RequestBody MagicItem magicItem) {
-        return magicItemService.createMagicItem(magicItem);
+    @Operation(
+        summary = "Create magic item", 
+        description = "Create a new magic item in the system", tags = {"Magic Items" }, 
+        requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Object containing the magic item's data", 
+            content = @Content(mediaType = "application/json", 
+            examples = @ExampleObject(
+                value = "{\"name\": \"Diamond Armor\", \"type\": \"ARMOR\", \"power\": 0, \"defence\": 2}"
+                )
+            )
+        ),
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Item created",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = MagicItem.class)
+                )
+            )
+        }
+    )
+    public ResponseEntity<Object> createItem(@RequestBody MagicItem magicItem) {
+        try {
+            return ResponseEntity.ok(magicItemService.createMagicItem(magicItem));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
-
-    
-
 }
