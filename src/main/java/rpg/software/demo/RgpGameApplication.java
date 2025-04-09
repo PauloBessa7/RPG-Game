bb931f33b0fac26b3f7347ec0504668890b432c6
PauloBessa7 <pauloh.bess@gmail.com>
2025-04-08 21:23:30 -0300

    primeiro commit

---
+++ src/main/java/rpg/software/demo/RgpGameApplication.java
@@ -0,0 +1,13 @@
+package rpg.software.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgpGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgpGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rpg/software/demo/RgpGameApplicationTests.java
@@ -0,0 +1,13 @@
+package rpg.software.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgpGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
